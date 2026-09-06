aa62e86793a8324e7998e0bc586243799aa6168e
Zolboo Batbaatar <zolboobatbaatar8@gmail.com>
2026-09-06 23:34:58 +0800

    Implement array utility functions in functions.c

--- functions.c
+++ functions.c
@@ -17,37 +17,109 @@ void print(int A[], int n)
 
 int max(int A[], int n)
 {
-        // Энд функцыг хэрэгжүүл
+        int max = A[0];
+        for (int i = 0; i < n; i++){
+                if(A[i]>max){
+                        max = A[i];        
+                }
+        }
+        return max;
 }
 
 int min(int A[], int n)
 {
-        // Энд функцыг хэрэгжүүл        
+        int min = A[0];
+        for(int i=0; i<n; i++){
+                if(A[i]>min){
+                         min = A[i];
+                }
+        }        
+        return min;       
 }
 
 void copy(int A[], int n, int B[])
 {
-        // Энд функцыг хэрэгжүүл        
+        for (int i = 0; i < n; i++){
+                B[i] = A[i];
+        }        
 }
 
 int find(int A[], int n, int x)
 {
-        // Энд функцыг хэрэгжүүл        
+        for (int i = 0; i < n; i++)
+        {
+                if(x == A[i]){
+                        return i;
+                }
+        }
+        return -1;        
 }
 
 int make_set(int A[], int n, int B[])
 {
-        // Энд функцыг хэрэгжүүл        
+        int m=0;
+        for (int i = 0; i < n; i++)
+        {
+                int found=0;
+                        for(int j = 0; j<m; j++){
+                                
+                                if(A[i]==B[j]){
+                                        found = 1;
+                                        break;
+                                }
+                        }
+                if(found == 0){
+                        B[m] = A[i];
+                        m++;
+                }
+                
+        }
+        return m;        
 }
 
 int union_set(int A[], int n, int B[], int m)
 {
-        // Энд функцыг хэрэгжүүл        
-}
+for (int i = 0; i < m; i++)
+        {
+                int found = 0;
+                        for (int k = 0; k < n; k++)
+                        {
+                                if (B[i] == A[k]){
+                                        found = 1;
+                                        break;
+                                }
+        }
+
+        if (found == 0){
+            A[n] = B[i];
+            n++;
+        }
+    }
+
+    return n;} 
 
 int intersection_set(int A[], int n, int B[], int m, int C[])
 {
-        // Энд функцыг хэрэгжүүл
+       int k = 0;
+        for (int i = 0; i < n; i++){
+                int found = 0;
+
+                for (int j = 0; j < m; j++)
+                {
+                if (A[i] == B[j]){
+                        found = 1;
+                         break;
+                        }
+                        }
+
+        if (found == 1)
+        {
+            C[k] = A[i];        
+            k++;
+        }
+    }
+
+    return k;
         
 }
 
